a4c6198275ab126fc04564d533fdec49e9b31551
hideonSOS <code1555@icloud.com>
2022-04-01 21:04:16 +0900

    test

--- WorkApp/models.py
+++ WorkApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class test_database1(models.Model):
+    id = models.AutoField(primary_key=True)
+    str_area1 = models.CharField(max_length=10)
+    str_area2 = models.CharField(max_length=100)
+    date_field=models.DateField()
+
