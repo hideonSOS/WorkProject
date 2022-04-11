9354dc7dfd71eb7bebd3cb2ea4d9bef0edf22cc2
hideonSOS <code1555@icloud.com>
2022-04-11 15:51:31 +0900

    test

--- WorkApp/models.py
+++ WorkApp/models.py
@@ -12,3 +12,10 @@ class test_database2(models.Model):
     str_areaon1 = models.CharField(max_length=10)
     str_areaon2 = models.CharField(max_length=100)
     date_fieldon=models.DateField()
+
+class Series(models.Model):
+    id= models.AutoField(primary_key=True)
+    title = models.CharField(max_length=30)
+    Syusai = models.CharField(max_length=5)
+    start_day=models.DateField()
+    end_day=models.DasteField()
