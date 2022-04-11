8553e6a072d8ae87ab9669372218a02a046aa1e5
hideonSOS <code1555@icloud.com>
2022-04-11 15:59:54 +0900

    test

--- WorkApp/admin.py
+++ WorkApp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import test_database1
+from .models import Series, test_database1
 from .models import test_database2
+
 
 admin.site.register(test_database1)
 admin.site.register(test_database2)
+admin.site.register(Series)
