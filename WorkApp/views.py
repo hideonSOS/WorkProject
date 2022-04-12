f0dc9968273df3700f7f6def291318f89ae4041f
hideonSOS <code1555@icloud.com>
2022-04-13 06:53:08 +0900

    no message

--- WorkApp/views.py
+++ WorkApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import InputForm
 from .models import Series, test_database1
 from django.contrib.auth.views import LoginView,LogoutView
@@ -10,10 +10,19 @@ def home(request):
 def page1(request):
     return render(request, 'WorkApp/page1.html')
 
-def input(request):
-    form = InputForm()
-    return render(request, 'WorkApp/input.html', {'form':form})
+def input_form(request):
+    #form = InputForm()
+    #return render(request, 'mlapp/input_form.html', {'form':form})
 
+    if request.method == "POST":            # Formの入力があった時、
+        form = InputForm(request.POST)      # 入力データを取得する。
+        if form.is_valid():                 # Formの記載の検証を行い、
+            form.save()                     # 問題なければ、入力データを保存
+            return redirect('page1')        # 保存後遷移するページの指定
+    else:
+        form = InputForm()
+        return render(request, 'WorkApp/input.html', {'form':form})
+        
 def return_database1(request):
     database1=test_database1.objects.all()
     Serieson = Series.objects.all()
