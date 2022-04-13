e107f242dbd5b3bb7802dada19e47fe662a2206c
hideonSOS <code1555@icloud.com>
2022-04-13 10:34:08 +0900

    no message

--- WorkApp/urls.py
+++ WorkApp/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns=[
     path('', views.home, name='home'),
     path('page1/', views.page1, name='page1'),
+    path('page2/', views.page2, name='page2'),
+    path('page3/', views.page3, name='page3'),
+    path('page4/', views.page4, name='page4'),
     path('input/', views.input, name='input'),
     path('output/', views.return_database1, name='output'),
     path('logout/', views.Logout.as_view(), name = 'logout'),
