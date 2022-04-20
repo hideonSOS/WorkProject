4223a51116c712805656ff3f3a82b489dfc70326
hideonSOS <code1555@icloud.com>
2022-04-20 09:22:32 +0900

    test

--- WorkApp/urls.py
+++ WorkApp/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns=[
     path('', views.home, name='home'),
     path('page1/', views.page1, name='page1'),
-    path('graph/', views.graph, name='graph'),
-    path('page3/', views.page3, name='page3'),
-    path('page4/', views.page4, name='page4'),
+    path('graph1/', views.graph1, name='graph1'),
+    path('graph2/', views.graph2, name='graph2'),
+    path('graph3/', views.graph3, name='graph3'),
     path('input/', views.input, name='input'),
     path('input2/', views.input2, name='input2'),
     path('output/', views.output, name='output'),
--- WorkApp/views.py
+++ WorkApp/views.py
@@ -14,48 +14,30 @@ def page1(request):
     return render(request, 'WorkApp/page1.html')
 
 @login_required
-def graph(request):
+def graph1(request):
     dicton = seiri_database()
     
-    return render(request, 'WorkApp/graph.html', {'dict':dicton})
+    return render(request, 'WorkApp/graph1.html', {'dict':dicton})
 
 @login_required
-def page3(request):
+def graph2(request):
     df = print_database()
     dict = df.to_dict('records')
     
-    return render(request, 'WorkApp/page3.html',{'dict':dict})
+    return render(request, 'WorkApp/graph2.html',{'dict':dict})
+
+@login_required
+def graph3(request):
+    df = print_database()
+    dict = df.to_dict('records')
+    
+    return render(request, 'WorkApp/graph3.html',{'dict':dict})
 
 @login_required
 def page4(request):
     return render(request, 'WorkApp/page4.html')
 
-# def input(request):
-#     #form = InputForm()
-#     #return render(request, 'mlapp/input_form.html', {'form':form})
 
-#     if request.method == "POST":            # Formの入力があった時、
-#         form = InputForm(request.POST)      # 入力データを取得する。
-#         if form.is_valid():                 # Formの記載の検証を行い、
-#             form.save()                     # 問題なければ、入力データを保存
-#             return redirect('page1')        # 保存後遷移するページの指定
-#     else:
-#         form = InputForm()
-#         return render(request, 'WorkApp/input.html', {'form':form})
-
-        
-# import pandas as pd
-# from django_pandas.io import read_frame      
-# def return_database1(request):
-#     database1=test_database1.objects.all()
-#     Serieson = Series.objects.all()
-#     df = read_frame(Serieson)
-#     print(df)
-#     database = {
-#         'database1':database1,
-#         'Series':Serieson
-#     }
-#     return render(request, 'WorkApp/output.html', {'database':database})
 @login_required
 def input(request):
     id = request.POST.get('id')
