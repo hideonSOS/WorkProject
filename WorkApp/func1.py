5c322f8bc2036cce60bd8343d1c271d6e63d7770
hideonSOS <code1555@icloud.com>
2022-04-20 19:29:49 +0900

    test

--- WorkApp/func1.py
+++ WorkApp/func1.py
@@ -64,7 +64,7 @@ def print_database():
     df.sort_values('id',inplace=True)
     connection.commit()
     connection.close()
-    df['end_day2']=[Weekday(i) for i in df['end_day']]
+    df['end_day2']=[i for i in df['end_day']]
 
     return df
 
