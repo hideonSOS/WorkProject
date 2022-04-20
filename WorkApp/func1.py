88018acb0822a84dcced4bff698ff1ac8d264ac5
hideonSOS <code1555@icloud.com>
2022-04-20 18:57:11 +0900

    test

--- WorkApp/func1.py
+++ WorkApp/func1.py
@@ -51,9 +51,8 @@ def delete_database(id):
 def Weekday(day):
     from datetime import datetime
     dt = datetime(day)
-    no = dt.weekday()
     w_list = ['(月)', '(火)', '(水)', '(木)', '(金)', '(土)', '(日)']
-    data = str(w_list[no])
+    data = w_list[dt.weekday()]
     return data
 
 #r4_table（請求データベース接続）
